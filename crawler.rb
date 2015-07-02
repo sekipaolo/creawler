2cb9c75ad195bec699f73c9761a75ce3a94c9866
Paolo Sechi <sekipaolo@gmail.com>
2015-07-02 15:26:10 +0200

    Write a clean report file

--- crawler.rb
+++ crawler.rb
@@ -9,61 +9,74 @@ class Crawler
   def initialize url
     @base_url = url
     uri = URI.parse(@base_url)
-    @logger_ok = Logger.new("logs/#{uri.host}.ok.log")
+    @report_file = File.open("logs/#{uri.host}.report.log", 'w+')
     @logger_err = Logger.new("logs/#{uri.host}.error.log")
     @http = Net::HTTP.new(uri.host, uri.port)
-    @links = []
-    @results = {}
+    @links = {}
+    @results = []
+    @error_count = 0
   end
 
-  def add_link path
+  def add_link path, origin
     path.gsub!(Regexp.new("^#{@base_url}"), "")
     path.gsub('\"')
-    unless path.match(/^$|^#|^http|^mailto|\/redirect\?goto/) || (@results[path] && @results[path] > 0)      
-      @links << path 
+    unless path.match(/^$|^#|^http|^mailto|\/redirect\?goto/) || (@results.include?(path))      
+      @links[path] = [] unless @links[path] 
+      @links[path] << origin 
     end
   end
 
-  def parse path, get_links=false
+  def parse path, origins, get_links=false
     uri = URI.parse(path)
     request = Net::HTTP::Get.new(path)
     response = @http.request(request)
     code = response.code.to_i
-    @results[path] = code
+    @results << path if code < 400
     if get_links
-      if code < 400
-        @logger_ok.debug "#{code} : #{path}"
+      if code < 400                
         doc = Nokogiri::HTML(response.body)
         doc.css('a').each do |node|
           # insert the link        
           link = node['href']
           next unless link
-          add_link link             
+          add_link link, path             
         end
+        puts "parsed #{path}"
       else 
+        puts "failed #{path}"
+        @error_count = +1
         @logger_err.error "#{code}: #{path}"
+        @logger_err.error "    COMING FROM: #{origins.join(' , ')}"
       end 
     end
   end  
 
   def go
-    parse '/', true
-    @links.uniq!
-    @logger_ok.info "Level 1: found #{@links.count} links: parsing its"
-    @links.each do |link|
-      @links.delete(link)
-      parse link, true
+    parse '/', [], true
+    #@links.uniq!
+    puts  "Level 1: found #{@links.count} links: parsing its"
+    @links.clone.each do |link, origins|
+      @links.delete link
+      parse link, origins, true
     end
-    @links.uniq!
-    @logger_ok.info "Level 2: found #{@links.count} links: parsing its"
-    @links.each do |link|
-      @links.delete(link)
-      parse link
+    #@links.uniq!
+    puts "Level 2: found #{@links.count} links: parsing its"
+    remaining = @links.count 
+
+    @links.clone.each do |link, origins|
+      remaining = remaining - 1
+      puts "#{remaining} remaining links: doing #{link}"
+      @links.delete link
+      parse link, origins
     end
-    @results.each do |url, code|
-      @logger_ok.info "#{code} #{url}"
+
+    @report_file.write "SUCCESS: #{@results.count} - ERRORS: #{@error_count}\n"
+    @results.sort!  
+    @results.each do |url|
+      @report_file.write "#{url}\n"
       puts url
     end
+    @report_file.close
   end
 
 end
